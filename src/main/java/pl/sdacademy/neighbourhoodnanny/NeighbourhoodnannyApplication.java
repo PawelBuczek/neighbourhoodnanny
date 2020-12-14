6d55f7c5dbbb2f643d3610c8899d45450ae56481
PawelBuczek <51830329+PawelBuczek@users.noreply.github.com>
2020-12-14 20:09:26 +0100

    Initial commit

---
+++ src/main/java/pl/sdacademy/neighbourhoodnanny/NeighbourhoodnannyApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.neighbourhoodnanny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighbourhoodnannyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeighbourhoodnannyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sdacademy/neighbourhoodnanny/NeighbourhoodnannyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.neighbourhoodnanny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighbourhoodnannyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
