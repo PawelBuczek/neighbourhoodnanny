0f4f99bf9ef112360f58b0ae2ef40c9d491c93a8
Sharin6 <agaberg@op.pl>
2020-12-14 21:28:31 +0100

    create entity - Child

---
+++ src/main/java/pl/sdacademy/neighbourhoodnanny/pl/sdacademy/childUser/Child.java
@@ -0,0 +1,67 @@
+package pl.sdacademy.neighbourhoodnanny.pl.sdacademy.childUser;
+
+import javax.persistence.*;
+
+@Entity
+public class Child {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    //ManytoMany?
+
+    public Child() {
+    }
+
+    public Child(int id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Child{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
