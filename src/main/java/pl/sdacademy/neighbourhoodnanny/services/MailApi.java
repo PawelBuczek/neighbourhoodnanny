e16c68a4b610175f78376bc617b215f14f53e451
Pawel Buczek <pawel.buczek@email.com>
2021-01-21 19:02:16 +0100

    remind password added

---
+++ src/main/java/pl/sdacademy/neighbourhoodnanny/services/MailApi.java
@@ -0,0 +1,43 @@
+package pl.sdacademy.neighbourhoodnanny.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import pl.sdacademy.neighbourhoodnanny.babysitter.Babysitter;
+import pl.sdacademy.neighbourhoodnanny.babysitter.BabysitterRepository;
+import pl.sdacademy.neighbourhoodnanny.users.User;
+import pl.sdacademy.neighbourhoodnanny.users.UserRepository;
+
+import javax.mail.MessagingException;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/mailer")
+public class MailApi {
+    BabysitterRepository babysitterRepository;
+    UserRepository userRepository;
+    private final MailService mailService;
+
+    @Autowired
+    public MailApi(MailService mailService, BabysitterRepository babysitterRepository, UserRepository userRepository) {
+        this.mailService = mailService;
+        this.babysitterRepository=babysitterRepository;
+        this.userRepository=userRepository;
+    }
+
+    @GetMapping("/RemindPassword/{email}")
+    public String sendMail(@PathVariable String email) throws MessagingException {
+        Babysitter babysitter = babysitterRepository.findByEmail(email).orElse(null);
+        assert babysitter != null;
+        User user = babysitter.getUser();
+        assert user != null;
+
+        mailService.sendMail(babysitter.getEmail(),
+                "Przypomnienie hasla dla: " + user.getUsername(),
+                "<b>Twoja rola to: </b>" + user.getRole()  +"<br><b>Twoje haslo to: </b>" + user.getPassword(), true);
+        return "wysłano";
+    }
+
+}
---
+++ src/main/java/pl/sdacademy/neighbourhoodnanny/services/MailService.java
@@ -0,0 +1,47 @@
+package pl.sdacademy.neighbourhoodnanny.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+@Service
+public class MailService {
+
+    public void sendMail(String to,
+                         String subject,
+                         String text,
+                         boolean isHtmlContent) throws MessagingException {
+        JavaMailSender javaMailSender = getJavaMailSender();
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(text, isHtmlContent);
+        javaMailSender.send(mimeMessage);
+    }
+
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("automailer.babysitter@gmail.com");
+        mailSender.setPassword("Mordor#13");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+        return mailSender;
+    }
+}
